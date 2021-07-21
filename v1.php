0647d034dcd23e07ef961f3ea21d18f2ab6a3306
Filioreanu Marius <62004975+MariusF233@users.noreply.github.com>
2021-07-21 19:13:49 +0300

    Add files via upload

--- v1.php
+++ v1.php
@@ -7,9 +7,8 @@ $i=0;
 $i2=0;
 $n=0;
 $months=['Ian','Feb','Mar','Apr','Mai','Feb','Iun','Iul','Aug','Sep','Oct','Nov','Dec'];
- foreach($months as $month)
- {
- if($month=='Feb') {
+ foreach($months as $month){
+ if($month == 'Feb') {
     $i++;
     }
  }
@@ -17,23 +16,27 @@ $months=['Ian','Feb','Mar','Apr','Mai','Feb','Iun','Iul','Aug','Sep','Oct','Nov'
 
 
 
-for($i2=0;$i2<20;$i2++)
-{
+for($i2 = 0; $i2 < 20; $i2++){
     $string[$i2]=rand(1,20);
 }
-for($i2=0;$i2<20;$i2++)
-{
-    if($string[$i2]==3){
+/*for($i2 = 0; $i2 < 20; $i2++){
+    if($string[$i2] == 3){
         break;}
     else{
 echo "   ".$string[$i2];
     }
+}*/
+$i2 = 0;
+while(($string[$i2] != 3)&&($i2 < 19)){
+
+    echo "   ".$string[$i2];
+    $i2++;
+
 }
 $i3=0;
 $n=count($months);
 
-for($i2=$n-1;$i2>=0;$i2--)
-{
+for($i2 = $n-1; $i2 >= 0; $i2--){
 
 $monthsReverse[$i3++]=$months[$i2];
 echo $monthsReverse[$i3-1];
@@ -43,12 +46,10 @@ function bubbleSort(string $string):void
 {$swapped=false;
     $aux='';
     $n=count($string);
-    while($swapped==false)
-    {
+    while($swapped == false){
         $swapped=false;
-        for($i=1;$i<=$n-1;$i++)
-        {
-         if($string[$i-1]>$string[$i]) {
+        for($i=1; $i <= $n-1; $i++){
+         if($string[$i-1] > $string[$i]) {
              $aux=$string[i-1];
              $string[$i-1]=$string[$i];
              $string[$i]=$aux;
