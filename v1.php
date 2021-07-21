3373255cc1a4fdfabf188a492e2340ea2c270803
Filioreanu Marius <62004975+MariusF233@users.noreply.github.com>
2021-07-21 18:37:57 +0300

    Add files via upload

--- v1.php
+++ v1.php
@@ -7,9 +7,9 @@ $i=0;
 $i2=0;
 $n=0;
 $months=['Ian','Feb','Mar','Apr','Mai','Feb','Iun','Iul','Aug','Sep','Oct','Nov','Dec'];
- foreach($months as $month){
- if($month=='Feb') 
-   {
+ foreach($months as $month)
+ {
+ if($month=='Feb') {
     $i++;
     }
  }
@@ -23,8 +23,8 @@ for($i2=0;$i2<20;$i2++)
 }
 for($i2=0;$i2<20;$i2++)
 {
-    if($string[$i2]==3)
-    {break;}
+    if($string[$i2]==3){
+        break;}
     else{
 echo "   ".$string[$i2];
     }
@@ -39,7 +39,7 @@ $monthsReverse[$i3++]=$months[$i2];
 echo $monthsReverse[$i3-1];
 }
 
-function bubbleSort($string)
+function bubbleSort(string $string)
 {$swapped=false;
     $aux='';
     $n=count($string);
@@ -48,8 +48,7 @@ function bubbleSort($string)
         $swapped=false;
         for($i=1;$i<=$n-1;$i++)
         {
-         if($string[$i-1]>$string[$i])
-         {
+         if($string[$i-1]>$string[$i]) {
              $aux=$string[i-1];
              $string[$i-1]=$string[$i];
              $string[$i]=$aux;
